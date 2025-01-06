8a6970a1b86459d687a39a92f4c7ffea35f2b69b
chaeyoung103 <thdcodud103@naver.com>
2025-01-06 20:46:08 +0900

    feat: button component

---
+++ plzdrawing/src/components/button/PrimaryButton.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  TouchableOpacityProps,
+} from "react-native";
+import Txt from "../text/Txt";
+import colors from "@/src/constants/Colors";
+
+interface PrimaryButtonProps extends TouchableOpacityProps {
+  title: string;
+  isBorder?: boolean;
+  color?: keyof typeof colors.button;
+}
+
+export const PrimaryButton = ({
+  title,
+  color = "primaryBackground",
+  ...rest
+}: PrimaryButtonProps) => {
+  return (
+    <TouchableOpacity
+      {...rest}
+      style={[styles.button, { backgroundColor: colors.button[color] }]}
+    >
+      <Txt style={styles.text} size={16}>
+        {title}
+      </Txt>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#FFE18D",
+    borderRadius: 12,
+    alignItems: "center",
+  },
+  border: {
+    borderWidth: 2,
+    borderColor: "white",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "regular",
+    color: "black",
+  },
+});
