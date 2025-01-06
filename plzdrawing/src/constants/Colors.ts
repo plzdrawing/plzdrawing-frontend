3047bc087a76c9ef7073f4b06d6af8d0df3e9b02
chaeyoung103 <thdcodud103@naver.com>
2025-01-06 20:45:00 +0900

    feat: add color system

--- plzdrawing/src/constants/Colors.ts
+++ plzdrawing/src/constants/Colors.ts
@@ -1,19 +1,30 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+  colors: {
+    text: "#000000", // Black
+    background: "#FFFFFF", // White
+    orange: "#FFA011", // 강조색_orange
+    yellow1: "#FFC311", // 메인색_yellow1
+    yellow2: "#FFE18D", // 서브색_yellow2
+    green1: "#5ECC90", // 보조색_green
+    green2: "#87DFAF", // 보조색_green2
+    error: "#FF727E", // 주의_red
+    gray1: "#F9F9F9", // 연회색1
+    gray2: "#D9D9D9", // 연회색2
+    gray3: "#E0DFDF", // 연회색3
+    grayDark1: "#A8A8A8", // 진회색01
+    grayDark2: "#828282", // 진회색02
+    black: "#000000", // Black
+    white: "#FFFFFF", // White
   },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+  tab: {
+    iconDefault: "#767A7A", // 연회색2
+    iconSelected: "#000000", // 강조색_orange
+  },
+  button: {
+    primaryBackground: "#FFC311", // 강조색_orange
+    primaryText: "#000000", // White
+    secondaryBackground: "#FFE18D", // 매연색_yellow1
+    disabledText: "#ffffff", // Black
+    disabledBackground: "#767A7A", // 연회색2
   },
 };
