3ef14a7346771a64da40012347bcbff1c07f6a49
chaeyoung103 <thdcodud103@naver.com>
2025-01-21 19:53:24 +0900

    fix: styled component로 변경

--- plzdrawing/src/screens/auth/LoginSplash.tsx
+++ plzdrawing/src/screens/auth/LoginSplash.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet, View } from "react-native";
-import { PrimaryButton } from "@/src/components/common/button/PrimaryButton";
 import colors from "@/src/constants/Colors";
 import Header from "@/src/components/common/header/Header";
 import React from "react";
@@ -7,54 +6,41 @@ import Txt from "@/src/components/common/text/Txt";
 import { BottomFixedArea } from "@/src/components/common/area/BottomFixedArea";
 import { useNavigation, NavigationProp } from "@react-navigation/native";
 import { RootStackParamList } from "@/src/types/navigation";
+import { Container } from "@/src/components/common/container/Container";
+import PrimaryButton from "@/src/components/common/button/PrimaryButton";
+import styled from "styled-components/native";
 
 export default function LoginSplash() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const [textLog, setTextLog] = React.useState("로그인이 필요해요.");
   const handleStartButtonOnClick = () => {
-    console.log("first time login");
     navigation.navigate("Login");
   };
 
   return (
-    <View style={styles.container}>
+    <Container>
       <Header />
       <Txt
-        size={24}
-        weight="bold"
+        variant="headLineBold"
         align="left"
-        lineheight={31}
         style={{ marginTop: 43, paddingLeft: 32 }}
       >
-        오랜만이에요 :) <br /> <br />
-        소일거리 드로잉어플과 함께 <br />
+        오랜만이에요 :)
+        {"\n"}
+        {"\n"}
+        소일거리 드로잉어플과 함께 {"\n"}
         소소한 일상을 즐겨보아요!
       </Txt>
       <BottomFixedArea>
-        <View style={styles.separator} />
-        <View style={styles.buttonContainer}>
+        <ButtonContainer>
           <PrimaryButton title="시작하기" onClick={handleStartButtonOnClick} />
-        </View>
+        </ButtonContainer>
       </BottomFixedArea>
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    backgroundColor: `${colors.colors.background}`,
-  },
-  buttonContainer: {
-    width: "100%",
-    paddingHorizontal: 57,
-    paddingBottom: 3,
-    paddingTop: 10,
-  },
-  separator: {
-    height: 0.5,
-    width: "100%",
-    backgroundColor: colors.colors.seperator,
-  },
-});
+const ButtonContainer = styled.View`
+  width: 100%;
+  padding: 10px 57px;
+`;
