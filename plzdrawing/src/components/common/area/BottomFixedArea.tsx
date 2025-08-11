7d518aaa89fb0f6a37265626c78e9fb23d32a627
junghyungo <pp90971658@gmail.com>
2025-08-11 19:30:20 +0900

    [init]: 다시 nativewind v4로 변경

--- plzdrawing/src/components/common/area/BottomFixedArea.tsx
+++ plzdrawing/src/components/common/area/BottomFixedArea.tsx
@@ -1,13 +1,62 @@
-import Colors from "@/src/constants/Colors";
-import React, { useMemo, useState } from "react";
+// import Colors from "@/src/constants/Colors";
+// import React, { useMemo, useState } from "react";
+// import {
+//   ViewStyle,
+//   StyleProp,
+//   Dimensions,
+//   Keyboard,
+//   Platform,
+// } from "react-native";
+// import styled from "styled-components/native";
+
+// interface BottomFixedAreaProps {
+//   children: React.ReactNode;
+// }
+// const { width } = Dimensions.get("window");
+
+// export const BottomFixedArea: React.FC<BottomFixedAreaProps> = ({
+//   children,
+// }) => {
+//   React.useEffect(() => {
+//     const showSubscription = Keyboard.addListener("keyboardDidShow", (e) => {});
+
+//     const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {});
+
+//     return () => {
+//       showSubscription.remove();
+//       hideSubscription.remove();
+//     };
+//   }, []);
+
+//   return (
+//     <FixedAreaContainer behavior={"padding"}>
+//       <ContentContainer>{children}</ContentContainer>
+//     </FixedAreaContainer>
+//   );
+// };
+
+// const FixedAreaContainer = styled.KeyboardAvoidingView`
+//   position: absolute;
+//   left: ${width * 0.5}px;
+//   right: auto;
+//   bottom: 0;
+//   width: 100%;
+//   transform: translateX(${-width * 0.5}px);
+//   max-width: 840px;
+//   z-index: 100;
+//   flex-direction: column;
+// `;
+
+// const ContentContainer = styled.SafeAreaView``;
+
+import React, { useEffect } from "react";
 import {
-  ViewStyle,
-  StyleProp,
   Dimensions,
   Keyboard,
   Platform,
+  KeyboardAvoidingView,
+  SafeAreaView,
 } from "react-native";
-import styled from "styled-components/native";
 
 interface BottomFixedAreaProps {
   children: React.ReactNode;
@@ -17,9 +66,8 @@ const { width } = Dimensions.get("window");
 export const BottomFixedArea: React.FC<BottomFixedAreaProps> = ({
   children,
 }) => {
-  React.useEffect(() => {
+  useEffect(() => {
     const showSubscription = Keyboard.addListener("keyboardDidShow", (e) => {});
-
     const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {});
 
     return () => {
@@ -29,22 +77,11 @@ export const BottomFixedArea: React.FC<BottomFixedAreaProps> = ({
   }, []);
 
   return (
-    <FixedAreaContainer behavior={"padding"}>
-      <ContentContainer>{children}</ContentContainer>
-    </FixedAreaContainer>
+    <KeyboardAvoidingView
+      behavior={"padding"}
+      className="absolute bottom-0 z-50 flex-col w-full max-w-[840px] left-1/2 -translate-x-1/2"
+    >
+      <SafeAreaView>{children}</SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
-
-const FixedAreaContainer = styled.KeyboardAvoidingView`
-  position: absolute;
-  left: ${width * 0.5}px;
-  right: auto;
-  bottom: 0;
-  width: 100%;
-  transform: translateX(${-width * 0.5}px);
-  max-width: 840px;
-  z-index: 100;
-  flex-direction: column;
-`;
-
-const ContentContainer = styled.SafeAreaView``;
