901c182d9999469272721b214e545159247b86e8
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-09-03 16:32:01 +0900

    [ui]: add mypage detail page

---
+++ plzdrawing/src/screens/profile/mypage/AlarmSetting.tsx
@@ -0,0 +1,29 @@
+﻿import styled from "styled-components/native";
+import HomeDetailHeader from "@/src/components/home/detail/HomeDetailHeader";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "@/src/types/navigation";
+import Colors from "@/src/constants/Colors";
+
+type AlarmSettingProps = NativeStackScreenProps<
+  RootStackParamList,
+  "AlarmSetting"
+>;
+
+export default function AlarmSetting({ route, navigation }: AlarmSettingProps) {
+  return (
+    <Container>
+      <HomeDetailHeader title="알림 설정" onBackPress={() => navigation.goBack()} />
+      <ScrollContainer showsVerticalScrollIndicator={false}></ScrollContainer>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${Colors.colors.white};
+`;
+
+const ScrollContainer = styled.ScrollView`
+  width: 100%;
+  flex: 1;
+`;
