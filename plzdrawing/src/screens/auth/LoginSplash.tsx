353a168f94d9a33f2e7b0acbf5a857dfc08594f6
junghyungo <pp90971658@gmail.com>
2025-12-15 23:44:18 +0900

    setting: twrnc 설치

--- plzdrawing/src/screens/auth/LoginSplash.tsx
+++ plzdrawing/src/screens/auth/LoginSplash.tsx
@@ -1,45 +1,45 @@
 import React from "react";
 import { useNavigation, NavigationProp } from "@react-navigation/native";
-import styled from "styled-components/native";
+import tw from "twrnc";
+
 import { RootStackParamList } from "@/src/types/navigation";
 import Txt from "@/src/components/common/text/Txt";
 import { BottomFixedArea } from "@/src/components/common/area/BottomFixedArea";
 import { Container } from "@/src/components/common/container/Container";
 import PrimaryButton from "@/src/components/common/button/PrimaryButton";
 import { GreeNormal } from "@/assets/images";
+import { View } from "react-native";
 
 export default function LoginSplash() {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
   const handleStartButtonOnClick = () => {
     navigation.navigate("Signup");
   };
 
   return (
-    <Container style={{ alignItems: "center", justifyContent: "center" }}>
+    <Container style={tw`items-center justify-center`}>
       <GreeNormal />
+
       <Txt
         variant="mainTitleBold"
         align="center"
-        style={{ marginTop: 9, marginBottom: 40 }}
+        style={tw`mt-[9px] mb-10`}
       >
         환영합니다 :) {"\n"}{"\n"}
         '그리'와 함께 {"\n"}
         소소한 일상을 즐겨보아요!
       </Txt>
+
       <BottomFixedArea>
-        <ButtonContainer>
+        <View style={tw`w-full px-[57px] py-[10px]`}>
           <PrimaryButton
             title="시작하기"
             onClick={handleStartButtonOnClick}
             color="sub_yellow"
           />
-        </ButtonContainer>
+        </View>
       </BottomFixedArea>
     </Container>
   );
 }
-
-const ButtonContainer = styled.View`
-  width: 100%;
-  padding: 10px 57px;
-`;
